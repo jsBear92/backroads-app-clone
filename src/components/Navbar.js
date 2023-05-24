b01d277ca4d16764d45d9608bfb3e23e856110a4
Jaeseong Jeong <js104427@gmail.com>
2023-05-24 10:13:04 +0800

    changed navigation link instead of hard coding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import logo from '../images/logo.svg';
+import { pageLinks } from '../data';
 
 function Navbar() {
     return (
@@ -13,21 +14,16 @@ function Navbar() {
                     </div>
                     {/* <!-- left this comment on purpose --> */}
                     <ul className="nav-links" id="nav-links">
-                        <li>
-                        <a href="#home" className="nav-link"> home </a>
-                        </li>
-
-                        <li>
-                        <a href="#about" className="nav-link"> about </a>
-                        </li>
-
-                        <li>
-                        <a href="#services" className="nav-link"> services </a>
-                        </li>
-
-                        <li>
-                        <a href="#tours" className="nav-link"> tours</a>
-                        </li>
+                        {pageLinks.map((link) => {
+                            const { id, href, text } = link;
+                            return (
+                                <li key={id}>
+                                    <a href={href} className="nav-link">
+                                        {text}
+                                    </a>
+                                </li>
+                            )
+                        })}
                     </ul>
                     <ul className="nav-icons">
                         <li>
